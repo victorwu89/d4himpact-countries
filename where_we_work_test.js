a1ea32fc363bcbf2db684809a6e44261034d2a3a
Victor Wu <victorwu89@gmail.com>
2024-01-29 22:53:15 -0500

    Update where_we_work_test.js

--- where_we_work_test.js
+++ where_we_work_test.js
@@ -108,7 +108,7 @@ function drawVisualization() {
  var options = {
 	 'backgroundColor': {fill:'#f5f5f5',stroke:'#FFFFFF' ,strokeWidth:0 },
 	 'colorAxis':  {minValue: 0 , maxValue: 30,
-		 colors: ['#0094cc','#0094cc','#5e5d5d','#0094cc','#5e5d5d','#5e5d5d','#5e5d5d','#0094cc','#0094cc','#5e5d5d','#5e5d5d','#0094cb','#0094cc','#0094cb','#0094cc',
+		 colors: ['#d9d9d9','#d9d9d9','#d9d9d9','#d9d9d9','#d9d9d9','#d9d9d9','#d9d9d9','#d9d9d9','#d9d9d9','#5e5d5d','#5e5d5d','#0094cb','#0094cc','#0094cb','#0094cc',
 		 '#0094cc','#0094cc','#0094cc','#0094cc','#0094cc','#0094cc','#0094cc','#0094cc','#0094cc','#0094cc','#0094cc','#0094cc','#0094cc', '#0094cc', '#0094cc','#0094cc']},
 	 'domain': 'IN',
 	 'legend': 'none',
